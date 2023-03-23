6c1e56b740bfbe9bbb01edd55fe6a7d3f1200b12
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-03-23 17:20:03 +0530

    import math

---
+++ import math sin,tan,cos,pi.py
@@ -0,0 +1,8 @@
+import math
+print(math.sin(math.radians(30)))
+print(math.cos(math.radians(60)))
+print(math.tan(math.radians(45)))
+print(math.tau)
+print(math.log10(10))
+print(math.pi)
+print(math.e)
